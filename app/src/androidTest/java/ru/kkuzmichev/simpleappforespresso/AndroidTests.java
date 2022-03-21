1b5848a61d17a54231c96bd5ff03356f7d3e008a
grigkatrin <grig.katrin@gmail.com>
2022-03-22 00:09:52 +0300

    second task

--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/AndroidTests.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/AndroidTests.java
@@ -1,16 +1,34 @@
 package ru.kkuzmichev.simpleappforespresso;
 
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import android.view.View;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.espresso.IdlingRegistry;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.contrib.DrawerActions;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.matcher.BoundedMatcher;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasData;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.anyOf;
 
 @RunWith(AndroidJUnit4.class)
 public class AndroidTests {
@@ -26,5 +44,77 @@ public class AndroidTests {
     @Test
     public void textCheckNegative() {
         onView(withId(R.id.text_home)).check(matches(withText("Some wrong text")));
+    }
+
+    @Test
+    public void testIntent() {
+        Intents.init();
+        try {
+            openActionBarOverflowOrOptionsMenu(ApplicationProvider.getApplicationContext());
+        } catch (Exception ignored) {
+
+        }
+        onView(anyOf(withText(R.string.action_settings), withId(R.id.action_settings))).perform(click());
+        Intents.intended(hasData("https://google.com"));
+    }
+
+    @Before
+    public void registerIdlingResources() {
+        IdlingRegistry.getInstance().register(NewResource.idlingResource);
+    }
+
+    @After
+    public void unregisterIdlingResources() {
+        IdlingRegistry.getInstance().unregister(NewResource.idlingResource);
+    }
+
+    @Test
+    public void testGalleryList() {
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+        onView(withId(R.id.nav_gallery)).perform(click());
+        onView(withId(R.id.recycle_view)).check(matches(CustomMatcher.withListSize(10)));
+    }
+
+    @Test
+    public void testItemElementDisplayed() {
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+        onView(withId(R.id.nav_gallery)).perform(click());
+        ViewInteraction recyclerView = onView(withId(R.id.recycle_view));
+        recyclerView.check(matches(CustomMatcher.atPositionItemVisible(0)));
+    }
+}
+
+class CustomMatcher {
+
+    public static Matcher<View> withListSize(final int size) {
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public boolean matchesSafely(final View view) {
+                return ((RecyclerView) view).getAdapter().getItemCount() == size;
+            }
+
+            @Override
+            public void describeTo(final Description description) {
+                description.appendText(size + " items are in a list");
+            }
+        };
+    }
+
+    public static Matcher<View> atPositionItemVisible(final int position) {
+        return new BoundedMatcher<View, RecyclerView>(RecyclerView.class) {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Item position is " + position + ": ");
+            }
+
+            @Override
+            protected boolean matchesSafely(final RecyclerView view) {
+                RecyclerView.ViewHolder viewHolder = view.findViewHolderForAdapterPosition(position);
+                if (viewHolder == null) {
+                    return false;
+                }
+                return viewHolder.itemView.getVisibility() == View.VISIBLE;
+            }
+        };
     }
 }
